9cc394a0bab00b0489b575ac088270d9733e1d87
micol <micol.benatti@gmail.com>
2025-04-18 12:01:38 +0200

    Creazione contatti e modifica degli assets

--- src/views/Contatti.vue
+++ src/views/Contatti.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="text-center text-xl">
-      Pagina Contatti
+  <div class="min-h-[calc(100vh-15rem)] flex flex-col items-center py-10 text-center">
+
+    <h1 class="text-2xl md:text-3xl font-bold uppercase text-[#2c1b13] mb-8 leading-tight">
+      Come contattare l'ufficio di Giangusto
+    </h1>
+
+    <div class="flex flex-col md:flex-row justify-center items-center gap-8 text-lg font-semibold text-[#2c1b13] mb-10">
+      <div>
+        <span class="font-bold">Tel:</span> +39 344 218 2874
+      </div>
+      <div>
+        <span class="font-bold">Email:</span> info@giangusto.it / amministrazione@giangusto.it
+      </div>
     </div>
-  </template>
+
+    <!-- Linea decorativa con icone -->
+    <div class="w-full bg-[#d98858] py-2 flex justify-center overflow-hidden">
+      <img src="../assets/icone.png" alt="Icone Divider" class="h-10 object-contain" />
+    </div>
+
+
+    <div class="mt-10">
+      <h2 class="text-2xl md:text-3xl font-bold uppercase text-[#2c1b13] mb-6 leading-tight">
+        Hai bisogno per QR code, poster o altre grafiche?
+      </h2>
+
+      <div class="text-lg font-semibold text-[#2c1b13]">
+        <span class="font-bold">Email:</span> grafica@giangusto.it
+      </div>
+    </div>
+  </div>
+</template>
